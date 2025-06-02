c9a27c89ed24b2ee844b9accd2bcf64810093dd3
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-02 23:36:12 +0600

    implement Pixabay context and state management

---
+++ pixabay-clone/src/context/PixabayContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const pixabayContext = createContext();
+export default pixabayContext;
---
+++ pixabay-clone/src/context/PixabayState.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PixabayContext from './PixabayContext'
+import { useEffect } from 'react';
+const PixabayState = (props) => {
+    const apiKey = "50640814-b8e31d536bcb1ae00eaec2858";
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=yellow+flowers&image_type=photo&per_page=100`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log(data.hits);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <PixabayContext.Provider value={{
+
+        }}>{props.children}</PixabayContext.Provider>
+    );
+};
+
+export default PixabayState
