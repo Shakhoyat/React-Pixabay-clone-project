219ab9122d3a0532d88e3b680fe5c6432b769afe
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-03 00:48:35 +0600

    feat: add NavBar component for navigation options

--- pixabay-clone/src/App.jsx
+++ pixabay-clone/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Imgs from './components/Imgs';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Imgs />
     </>
   )
---
+++ pixabay-clone/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className='container text-center my-3'>
+            <button type="button" className="btn btn-primary mx-3">Nature</button>
+            <button type="button" className="btn btn-secondary mx-3">Science</button>
+            <button type="button" className="btn btn-success mx-3">Computer</button>
+            <button type="button" className="btn btn-danger mx-3">Buildings</button>
+            <button type="button" className="btn btn-warning mx-3">Sports</button>
+            <button type="button" className="btn btn-info mx-3">Food</button>
+            <button type="button" className="btn btn-light mx-3">Travel</button>
+            <button type="button" className="btn btn-dark mx-3">Business</button>
+        </div>
+    )
+}
+
+export default NavBar
