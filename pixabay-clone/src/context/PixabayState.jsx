38ba0ae620b6f1e964530a6b422f1dd3cc613152
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-02 23:38:38 +0600

    implement image data fetching and context state management

--- pixabay-clone/src/context/PixabayState.jsx
+++ pixabay-clone/src/context/PixabayState.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import PixabayContext from './PixabayContext'
 import { useEffect } from 'react';
 const PixabayState = (props) => {
+    const [imgData, setimgData] = useState([]);
+
     const apiKey = "50640814-b8e31d536bcb1ae00eaec2858";
     useEffect(() => {
         const fetchData = async () => {
@@ -11,7 +13,7 @@ const PixabayState = (props) => {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                console.log(data.hits);
+                setimgData(data.hits);
             } catch (error) {
                 console.error('Fetch error:', error);
             }
@@ -21,7 +23,7 @@ const PixabayState = (props) => {
 
     return (
         <PixabayContext.Provider value={{
-
+            imgData
         }}>{props.children}</PixabayContext.Provider>
     );
 };
